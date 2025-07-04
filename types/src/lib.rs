9e958c712a828c401abac3421adbcd216526ec1e
SantiagoVira <santiagovira06@gmail.com>
2025-07-04 16:55:43 -0400

    idk if this actually works....

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::num::NonZeroUsize;
+use std::num::{NonZeroU8, NonZeroUsize};
 
 pub use rmp_serde;
 pub use serde;
@@ -11,6 +11,15 @@ pub struct Ballot {
     #[serde(skip)]
     pub selected: usize,
     pub votes: Vec<Option<Candidate>>,
+}
+
+type CandidateBetter = NonZeroU8;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct BallotBetter {
+    #[serde(skip)]
+    pub selected: u8,
+    pub votes: Vec<Option<CandidateBetter>>,
 }
 
 #[derive(Serialize, Deserialize)]
